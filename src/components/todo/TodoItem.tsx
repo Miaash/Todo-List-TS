f6f7221960f7330d76dd3e8c18bdf84f4ea00cff
Mia <girl0930h@gmail.com>
2023-01-05 03:10:35 +0900

    [FEAT]: todo 수정&삭제기능 구현

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,30 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { todoType } from '../../types/\btypes';
 import { todos } from '../../firebase/firebaseController';
-import { useDocId } from '../../store/store';
 
 interface IProps {
   todo: todoType;
 }
 
 const TodoItem = ({ todo }: IProps) => {
-  const { docId } = useDocId();
+  const [editMode, setEditMode] = useState(false);
+  const [newTodo, setNewTodo] = useState(todo.text);
+
+  // todo 삭제
   const removeHandler = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     const confirmMsg = window.confirm('삭제하시겠습니까?');
     if (confirmMsg) {
-      // 이렇게 하면 방금 추가한 todo의 doc id가 업데이트 되었기 때문에 방금 추가된 todo만 삭제할 수 있게 된다.
-      // 내가 선택한 요소의 docId를 가져와야한다.
-      await todos.doc(docId).delete();
+      // todo.id를 doc경로로 지정해 삭제
+      await todos.doc(`${todo.id}`).delete();
       alert('삭제되었습니다.');
     }
   };
+
+  // 수정 모드 전환
+  const onEditModeHandler = () => {
+    setEditMode((prev) => !prev);
+  };
+
+  // todo 수정
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await todos.doc(`${todo.id}`).update({
+      text: newTodo,
+    });
+    alert('수정되었습니다.');
+    setEditMode(false);
+  };
+  const onEditHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setNewTodo(e.target.value);
+  };
+
   return (
     <div>
-      <div>{todo.text}</div>
-      <div>
-        <button onClick={removeHandler}>삭제</button>
-      </div>
+      {editMode ? (
+        <div>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              value={newTodo}
+              required
+              placeholder="Edit your Todo."
+              onChange={onEditHandler}
+            />
+            <button>confirm</button>
+          </form>
+          <button onClick={onEditModeHandler}>취소</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{todo.text}</h3>
+          <div>
+            <button onClick={removeHandler}>삭제</button>
+            <button onClick={onEditModeHandler}>수정</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
