5107f1f104b8dbc8b7bfb709c8fcb74e1ecb6877
Mia <girl0930h@gmail.com>
2023-01-06 00:53:25 +0900

    [Edit]필요없는 파일 제거

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,4 @@
 import create from 'zustand';
-// import { apiKey } from "../firebase/firebaseConfig";
 
 export interface isLoginType {
   isLogin: boolean;
@@ -9,21 +8,4 @@ export interface isLoginType {
 export const useLoginStore = create<isLoginType>((set) => ({
   isLogin: false,
   setIsLogin: () => set(() => ({ isLogin: true })),
-}));
-
-// const _session_key = `firebase:authUser:${apiKey}:[DEFAULT]`;
-// const isToken = sessionStorage.getItem(_session_key);
-export const useTokenStore = create((set) => ({
-  token: sessionStorage.getItem('access_token'),
-}));
-
-export interface docIdType {
-  //타입스크립트 사용 시 필요 (js라면 지우기)
-  docId: string;
-  setDocId: (docId: string) => void;
-}
-
-export const useDocId = create<docIdType>((set) => ({
-  docId: '',
-  setDocId: (docId) => set({ docId }),
 }));
